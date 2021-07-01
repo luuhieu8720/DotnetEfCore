a9458cf763b0e53b427220996901e9c58dd7627f
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-01 11:24:51 +0700

    Add EF Core exercise 02

---
+++ EFCoreExercise/Exercises/Exercise2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExercise.Exercises
+{
+    public class Exercise2
+    {
+        public List<string> GetStudentsByYear(int year)
+        {
+            using var dbContext = new DataContextFactory().CreateDbContext(new string[0]);
+            return dbContext.Student.Where(s => s.Birthday.Year == year)
+                                    .Select(s => s.Name)
+                                    .ToList();
+        }
+
+    }
+}
