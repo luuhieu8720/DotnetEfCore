53f66ba1846dbde58b5a021c913400fd39eb39ed
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-01 11:42:02 +0700

    Add EF Core Exercise 13

---
+++ EFCoreExercise/Exercises/Exercise13.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExercise.Exercises
+{
+    public class Exercise13
+    {
+        public bool CheckStudent(int year, double score)
+        {
+            using var dbContext = new DataContextFactory().CreateDbContext(new string[0]);
+            return dbContext.Student
+                            .Any(s => s.Class.Name.Equals("Class A")
+                            && s.Birthday.Year == year
+                            && s.Score >= score);
+        }
+    }
+}
