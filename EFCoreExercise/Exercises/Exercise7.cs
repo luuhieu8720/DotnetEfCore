d5340795a07309e09898f69f608ca86f0150d5ad
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-01 11:32:51 +0700

    Add EF Core exercise 07

---
+++ EFCoreExercise/Exercises/Exercise7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExercise.Exercises
+{
+    public class Exercise7
+    {
+        public List<double?> FindAllScores()
+        {
+            using var dbContext = new DataContextFactory().CreateDbContext(new string[0]);
+            return dbContext.Student.Select(s => s.Score).Distinct().ToList();
+        }
+    }
+}
