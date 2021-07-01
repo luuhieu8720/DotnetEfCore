672ed746654707727230a4e11a638c25e76f4b6e
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-01 11:39:38 +0700

    Add EF Core exercise 11

---
+++ EFCoreExercise/Exercises/Exercise11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExercise.Exercises
+{
+    public class Exercise11
+    {
+        public string ClassHasHighestScore()
+        {
+            using var dbContext = new DataContextFactory().CreateDbContext(new string[0]);
+            
+            var innerJoin = from student in dbContext.Student
+                            join @class in dbContext.Class
+                            on student.Class.Id equals @class.Id
+                            select new { student, @class };
+            return innerJoin.AsEnumerable()
+                            .GroupBy(student => student.@class.Name)
+                            .Select(group => new { className = group.Key, score = group.Average(s => s.student.Score) })
+                            .OrderByDescending(dict => dict.score)
+                            .First().className;
+        }
+    }
+}
