bdbfefbca6a100a4f96c9e177c2e01cd1f7d4a8e
luuhieu8720 <lethiluuhieu@gmail.com>
2021-07-01 11:40:45 +0700

    Add EF Core Exercise 12

---
+++ EFCoreExercise/Exercises/Exercise12.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreExercise.Exercises
+{
+    public class Exercise12
+    {
+        public List<string> RandomFiveStudentByClass(string className)
+        {
+            using var dbContext = new DataContextFactory().CreateDbContext(new string[0]);
+            return dbContext.Student
+                            .Where(s => s.Class.Name.Equals(className))
+                            .Select(s => s.Name)
+                            .Take(5)
+                            .ToList();
+        }
+    }
+}
